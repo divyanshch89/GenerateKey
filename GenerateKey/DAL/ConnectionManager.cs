682aa3ba49e248f21e3b9fa4c04ce4eb9907fb78
Chaudhary <Divyansh.Chaudhary@cbre.com>
2018-08-20 17:08:48 -0500

    updated DAL

--- GenerateKey/DAL/ConnectionManager.cs
+++ GenerateKey/DAL/ConnectionManager.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
 
 namespace GenerateKey.DAL
 {
     public class ConnectionManager
     {
+        private DocumentClient client;
+        //TODO: read below properties from appsettings.json
+        private const string EndpointUrl = "<your endpoint URL>";
+        private const string PrimaryKey = "<your primary key>";
+        private const string DBName = "<someDBName>";
         public ConnectionManager()
         {
             //intialize Cosmos DB objects here
+            client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
+            client.CreateDatabaseIfNotExistsAsync(new Database { Id = DBName });
         }
-        public bool SaveWorkOrder()
+        public bool SaveWorkOrderAsync()
         {
             return false;
         }
